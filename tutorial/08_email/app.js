cada7e5f41af794d5c370df7fbfffdefc5e19e0e
KimHwiJin <gnwls2004@gmail.com>
2023-01-11 16:23:32 +0900

    email end

--- tutorial/08_email/app.js
+++ tutorial/08_email/app.js
@@ -1,19 +1,34 @@
+const dimm = document.querySelector('.dimm')
+const modal = document.querySelector('.modal')
+const closeBtn = document.querySelector('.close')
+const inputs = document.querySelectorAll('input')
+console.log(inputs)
+
 window.onload = function () {
     console.log('ffffff')
     document
         .getElementById('contact-form')
         .addEventListener('submit', function (event) {
             event.preventDefault()
+            dimm.classList.add('active')
             // generate a five digit number for the contact_number variable
             this.contact_number.value = (Math.random() * 100000) | 0
             // these IDs from the previous steps
             emailjs.sendForm('service_yimtmni', 'template_c32qtc8', this).then(
                 function () {
                     console.log('SUCCESS!')
+                    modal.classList.add('active')
                 },
                 function (error) {
                     console.log('FAILED...', error)
                 }
             )
         })
+
+    closeBtn.addEventListener('click', function () {
+        modal.classList.remove('active')
+        dimm.classList.remove('active')
+
+        inputs.forEach((item) => (item.value = ''))
+    })
 }

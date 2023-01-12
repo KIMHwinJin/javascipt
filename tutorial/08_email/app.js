4b55a52dcd27add8e5ce20f5ea5d8c14a772f36e
KimHwiJin <gnwls2004@gmail.com>
2023-01-12 15:49:42 +0900

    end

--- tutorial/08_email/app.js
+++ tutorial/08_email/app.js
@@ -2,14 +2,29 @@ const dimm = document.querySelector('.dimm')
 const modal = document.querySelector('.modal')
 const closeBtn = document.querySelector('.close')
 const inputs = document.querySelectorAll('input')
-console.log(inputs)
+
+const userName = document.getElementById('userName')
+const email = document.getElementById('email')
+const message = document.getElementById('message')
+
+// console.log(userName, email, message)
+// console.log(inputs)
 
 window.onload = function () {
-    console.log('ffffff')
     document
         .getElementById('contact-form')
         .addEventListener('submit', function (event) {
             event.preventDefault()
+
+            if (
+                userName.value == '' ||
+                email.value == '' ||
+                message.value == ''
+            ) {
+                alert('이름, 이메일, 내용을 확인하세요')
+                return
+            }
+
             dimm.classList.add('active')
             // generate a five digit number for the contact_number variable
             this.contact_number.value = (Math.random() * 100000) | 0
@@ -30,5 +45,6 @@ window.onload = function () {
         dimm.classList.remove('active')
 
         inputs.forEach((item) => (item.value = ''))
+        message.value = ''
     })
 }
